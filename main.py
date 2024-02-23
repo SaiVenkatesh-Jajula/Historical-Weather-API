a5a0121f303eef9b0d7e7dd8e594b879dfbe9bcc
saivenkatesh <saivenkatesh619@gmail.com>
2024-02-23 15:37:23 +0530

    Add temperature returning feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,10 +11,13 @@ def index():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temp = 47
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    # Get certain cell
+    temperature = df.loc[df["    DATE"] == date]['   TG'].squeeze() / 10
     return {"station": station,
             "date": date,
-            "Temperature": temp}
+            "Temperature": temperature}
 
 
 if __name__ == "__main__":
